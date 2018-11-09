a98acbd92f7a262baffdf70ad0568e5ba1ffb216
andrewsouthard1 <andrew.southard@datadoghq.com>
2018-11-09 15:08:19 -0500

    Made input an array

--- AgentFunction.ps1
+++ AgentFunction.ps1
@@ -1,17 +1,25 @@
 function AgentFunction {
-<#
     param(
       [Parameter(Mandatory)]
       [ValidateNotNullOrEmpty()]
-      [switch]$Input
+      [array]$argList
     )
-#>
 
     $installPath = (Get-ItemProperty -Path HKLM:\SOFTWARE\Datadog\Datadog' 'Agent\ -Name InstallPath)."InstallPath"
     $installPath = $installPath + 'embedded\agent.exe'
 
+    for($i=0; $i -lt $argList.length; $i++){
+    	if($i -eq 0) {
+	    	$cmdString = $argList[$i] + " "
+    	}
+    	elseif($i -eq ($argList.length-1)){
+    		$cmdString += $argList[$i]
+    	} 
+    	else {
+    		$cmdString += $argList[$i] + " "
+    	}
 
-    &$installPath 
+    }
 
-    $PSBoundParameters
+    &$installPath $cmdString
 }
--- DatadogAgent.ps1
+++ DatadogAgent.ps1
@@ -21,11 +21,53 @@ function DatadogAgent {
 
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$Input2
+        [string]$Input2,
 
+        [Parameter(Mandatory=$FALSE)]
+        [string]$Input3,
+
+        [Parameter(Mandatory=$FALSE)]
+        [string]$Input4
     )
+
+    <#
+    for ($i=1; $i -le $PSBoundParameters.count; $i++) {
+        Write-Output $i
+        
+        Write-Output $PSBoundParameters
+
+        if($PSBoundParameters["Input" + $i]){
+            Write-Output "There's a value"
+            Write-Output $PSBoundParamters[$i]
+        }
+    }
+    #>
+
+    <# 
+    foreach($boundparam in $PSBoundParameters.GetEnumerator()) {
+        $Input2 = $Input2 + $boundparam.Value
+    }
+    #>
+
+    $argList = @()
+
+    if($Input2) {
+        Write-Output "There is input2"
+        $argList += $Input2
+    }
+
+    if($Input3) {
+        Write-Output "There is input3"
+        $argList += $Input3
+    }
+
+    if($Input4) {
+        Write-Output "There is input4"
+        $argList += $Input4
+    }
+
     switch ( $Input1 ) {
-        "agent" { AgentFunction $Input2 }
+        "agent" { AgentFunction $argList }
         "show" { ShowFunction $Input2 }
     }
 }
