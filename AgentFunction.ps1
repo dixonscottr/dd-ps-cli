c14714295ca0a084e6e8f4f0f2e6b442e24757ba
andrewsouthard1 <andrew.southard@datadoghq.com>
2018-11-09 15:28:05 -0500

    Allow multiple args

--- AgentFunction.ps1
+++ AgentFunction.ps1
@@ -5,11 +5,13 @@ function AgentFunction {
       [array]$argList
     )
 
+    <# Assign the installed path of agent.exe #>
     $installPath = (Get-ItemProperty -Path HKLM:\SOFTWARE\Datadog\Datadog' 'Agent\ -Name InstallPath)."InstallPath"
     $installPath = $installPath + 'embedded\agent.exe'
 
+    <# Loop through input of command line args #>
     for($i=0; $i -lt $argList.length; $i++){
-    	if($i -eq 0) {
+    	if(($i -eq 0) -And ($i -ne ($argList.length-1))) {
 	    	$cmdString = $argList[$i] + " "
     	}
     	elseif($i -eq ($argList.length-1)){
@@ -21,5 +23,6 @@ function AgentFunction {
 
     }
 
-    &$installPath $cmdString
+    <# execute Agent! #>
+    &$installPath $cmdString.split()
 }
--- DatadogAgent.ps1
+++ DatadogAgent.ps1
@@ -30,39 +30,14 @@ function DatadogAgent {
         [string]$Input4
     )
 
-    <#
-    for ($i=1; $i -le $PSBoundParameters.count; $i++) {
-        Write-Output $i
-        
-        Write-Output $PSBoundParameters
-
-        if($PSBoundParameters["Input" + $i]){
-            Write-Output "There's a value"
-            Write-Output $PSBoundParamters[$i]
-        }
-    }
-    #>
-
-    <# 
-    foreach($boundparam in $PSBoundParameters.GetEnumerator()) {
-        $Input2 = $Input2 + $boundparam.Value
-    }
-    #>
-
     $argList = @()
-
     if($Input2) {
-        Write-Output "There is input2"
         $argList += $Input2
     }
-
     if($Input3) {
-        Write-Output "There is input3"
         $argList += $Input3
     }
-
     if($Input4) {
-        Write-Output "There is input4"
         $argList += $Input4
     }
 
