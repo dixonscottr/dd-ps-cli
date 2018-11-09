e9c0678b74dd04bf5c432ae61d6e74035919d977
Scott Dixon <dixon.scottr@gmail.com>
2018-11-09 11:52:13 -0500

    Put functions in own file

--- DatadogAgent.ps1
+++ DatadogAgent.ps1
@@ -1,3 +1,6 @@
+Import-module .\ShowFunction.ps1 -Force
+Import-module .\AgentFunction.ps1 -Force
+
 <#
 .SYNOPSIS
     use the Datadog Agent commands
@@ -25,58 +28,4 @@ function DatadogAgent {
         "agent" { AgentFunction $Input2 }
         "show" { ShowFunction $Input2 }
     }
-}
-
-function AgentFunction {
-    param(
-      [Parameter(Mandatory)]
-      [ValidateNotNullOrEmpty()]
-      [string]$Input2
-    )
-    Write-Output $Input2
-}
-
-function ShowFunction {
-    param(
-      [Parameter(Mandatory)]
-      [ValidateNotNullOrEmpty()]
-      [string]$Input2
-    )
-    
-    if ($Input2 -eq "datadog.yaml") {
-        OpenFileExplorer (GetProgramDataPath)
-    }  else {
-        OpenFileExplorer (GetPDAgentPaths $Input2)
-    }
-}
-
-function GetProgramDataPath {
-    $RepoPath = (Get-ItemProperty -Path HKLM:\SOFTWARE\Datadog\Datadog' 'Agent\ -Name TufMetaPath)."TufMetaPath"
-    $RepoCharLocation = $RepoPath.IndexOf("repositories\")
-    $ProgramDataPath = $RepoPath.Substring(0,$RepoCharLocation)
-    return $ProgramDataPath
-}
-
-function OpenFileExplorer {
-    param(
-      [Parameter(Mandatory)]
-      [ValidateNotNullOrEmpty()]
-      [string]$Path
-    )
-    if (Test-Path $Path) {
-        Write-Output "Opening directory in file explorer: $Path"
-        Invoke-Item $Path
-    } else {
-        Write-Output "This path does not exist: $Path"
-    }
-}
-
-function GetPDAgentPaths {
-    param(
-      [Parameter(Mandatory)]
-      [ValidateNotNullOrEmpty()]
-      [string]$Path
-    )
-    $GetProgramDataPath = GetProgramDataPath
-    return "$GetProgramDataPath\$Path"
 }
